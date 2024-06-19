67807a673e8b7e1708b31c753f5556682bf68eb0
EGARRISXN <egarrisxn@gmail.com>
2024-06-18 20:29:45 -0400

    updated font and robots, sitemap, and manifest!

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,21 @@
 import './globals.css'
-import {Inter} from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import { Roboto } from "next/font/google";
 import CartProvider from './components/Providers'
 import MyNavbar from './components/MyNavbar'
 import ShoppingCartModal from './components/ShoppingCartModal'
 import MyFooter from './components/MyFooter'
 
-const inter = Inter({subsets: ['latin']})
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
+  display: "swap",
+});
 
 export const metadata = {
   metadataBase: new URL('https://unofficialmerchandise.vercel.app'),
   title: 'Unofficial Merchandise',
-  description: 'The unofficial merch store for Unofficial Merchandise.',
+  description: 'The unofficial merchs tore for Unofficial Merchandise.',
   referrer: 'origin-when-cross-origin',
   keywords: ['sanity', 'next', 'react', 'tailwindcss', 'cms', 'ecommerce', 'egarrisxn'],
   robots: {
@@ -34,10 +39,10 @@ export const metadata = {
   },
 }
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <CartProvider>
           <MyNavbar />
           <ShoppingCartModal />
